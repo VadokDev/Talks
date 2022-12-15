cde3deea7d8044485e830d1af5e2c9bfae2bbd92
VadokDev <gonzalo.fdez06@gmail.com>
2022-12-15 09:42:42 -0300

    added new talks

--- Evaluando la Complejidad de una tarea en Python/assignments/t8.py
+++ Evaluando la Complejidad de una tarea en Python/assignments/t8.py
@@ -1,62 +1,21 @@
-def procesar_planilla(composers, works, sheet):
-    d = {}
-    artists_d = {}
-    comp = open(composers)
-    songs = open(works)
-    songsheet = open(sheet)
-    line1 = 0 # para solo usar la primera línea de los archivos de planilla
-    songscounter = 0 # contador de canciones total para después saber en cuánto repartir el total
-    for artists in comp:
-        templ = artists.strip().split(";")
-        artists_d[templ[0]] = templ[1]
-    for line in songsheet:
-        if line1 == 0:
-            eventdetails = line.strip().split(";")
-            line1 += 1
-        else:
-            songcode = line.strip()
-            if songcode not in d:
-                d[songcode] = songcode
-            songscounter += 1
-    budget = round(int(eventdetails[-1])/songscounter)
-    for songvalues in songs:
-        nlist = songvalues.strip().split(";")
-        if nlist[0] in d and d[nlist[0]] == nlist[0]:
-            d[nlist[0]] = []
-            d[nlist[0]].append(nlist[1])
-            nnlist = nlist[2:]
-            for artistcode1 in nnlist:
-                if artistcode1 in artists_d:
-                    artistname = artists_d[artistcode1]
-                    namebudgetlist = [artistname, round(budget/len(nnlist))]
-                d[nlist[0]].append(namebudgetlist)
-    comp.close()
-    songs.close()
-    songsheet.close()
-    return d
-d = {}
-arch = open("planillas.txt")
-newfile = open("liquidación.txt", "w")
-newfile.write("Liquidación por derechos de autor\n")
-for line in arch:
-    nline = line.strip()
-    d1 = procesar_planilla('autores.csv', 'obras.csv', nline)
-    for key in d1:
-        nlist = d1[key][1:]
-        for pos in range(0,len(nlist)):
-            name = nlist[pos][0]
-            money = nlist[pos][1]
-            if name not in d:
-                d[name] = []
-            d[name].append(money)
-nlist = []
-for name in d:
-    nlist.append([sum(d[name]),name])
-    nlist.sort(reverse=True)
-for money, name in nlist:
-    lol = "Compositor(a): {0}, Monto consolidado: ${1}.\n"
-    newfile.write(lol.format(name,money))
-arch.close()
-newfile.close()
-
-# Código hecho por Felipe Bustos, aka half_pipes.
+def obtener_valor(mano):
+    j = 0
+    i = 0
+    total = 0
+    while i < len(mano):
+        if mano[i] == "♥" or mano[i] == "♦" or mano[i] == "♠" or mano[i] == "♣":
+            numero = mano[j:i]
+            if numero == "J" or numero == "Q" or numero == "K":
+                numero = 10
+            elif numero == "A":
+                hay_A = True
+                numero = 11
+            else:
+                numero = int(numero)
+            total += numero
+            if total > 21 and hay_A == True:
+                total -= 10
+            j = i + 2
+            hay_A = False
+        i += 1
+    return total
---
+++ Potenciando Javascript con WebAssembly - un caso aplicado a la educacion/script.md
@@ -0,0 +1,26 @@
+## Presentación
+
+* Walmart STA
+* OCILabs
+* Ayudante Coordinador de Programación
+
+## Problema
+
+* Las tareas son complicadas de redactar
+* Saber si su dificultad se ajusta a lo esperado en el curso
+* Problemas de hacer malas tareas
+
+## Solución Técnica
+
+* Cómo evaluar la complejidad de una tarea
+* Ishvel
+* Multimetrics
+* WebAssembly
+* Pyodide
+* Implementación
+
+## Conclusiones
+
+* Ventajas y desventajas de hacer una aplicación autocontenida
+* Descubrimientos interesantes 
+* Mantención
