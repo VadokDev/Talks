d50a872b94a4111ceda8b66b601954d281d7112f
VadokDev <gonzalo.fdez06@gmail.com>
2022-12-21 17:23:27 -0300

    added more tests

--- Workshop Mantenibilidad y Testing en un Stack Javascript/examples/susblisher/src/__tests__/unit/Molecules/Message/index.test.js
+++ Workshop Mantenibilidad y Testing en un Stack Javascript/examples/susblisher/src/__tests__/unit/Molecules/Message/index.test.js
@@ -1,9 +1,21 @@
 import Message from '../../../../molecules/Message';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
 describe('Message molecule unit testing', () => {
   it('Should render a message with the given content', () => {
     const expectedContent = 'Hola';
     render(<Message content={expectedContent} />);
     expect(screen.getByText(expectedContent)).toBeInTheDocument();
+  });
+
+  it('Should call the handler when the button is clicked', () => {
+    const buttonText = 'Click me';
+    const onClickHandlerSpy = jest.fn();
+
+    render(<Message content={buttonText} onClickHandler={onClickHandlerSpy} />);
+    const button = screen.getByText(buttonText);
+    fireEvent.click(button);
+
+    expect(onClickHandlerSpy).toBeCalled();
   });
 });
